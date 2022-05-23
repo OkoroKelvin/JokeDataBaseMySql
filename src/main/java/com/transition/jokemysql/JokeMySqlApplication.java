5ef49e7058b19d64108c45fafe87ee1e473e9b64
OKORO KELVIN <okorokelvinemoakpooke@yahoo.com>
2022-05-23 15:17:13 +0100

    Project Setup

---
+++ src/main/java/com/transition/jokemysql/JokeMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.transition.jokemysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeMySqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeMySqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/transition/jokemysql/JokeMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transition.jokemysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeMySqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
